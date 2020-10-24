9d3a5a28795a0d1c0ce4b50a87dd8b45d08ebf5a
emilybonar <emilykbonar@gmail.com>
2020-10-24 11:52:20 -0500

    Made a splash screen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import Tree from "./Tree";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	let playlistEntered = false;
+	if (playlistEntered) {
+		return (
+			<div className="App">
+				<Tree />
+			</div>
+		);
+	} else {
+		return (
+			<div className="App">
+				<form className="playlist-form">
+					<input
+						className="playlist-input"
+						placeholder="Enter a Spotify playlist URL"
+					></input>
+					<button className="playlist-submit">Enter</button>
+				</form>
+			</div>
+		);
+	}
 }
 
 export default App;
