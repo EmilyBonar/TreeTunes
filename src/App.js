0ecec0b52c1c0b187e48ce3cd22cd832886faf7a
emilybonar <emilykbonar@gmail.com>
2020-10-24 16:11:00 -0500

    Got API response

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import Tree from "./Tree";
 import "./App.css";
 
+function getPlaylist(e) {
+	e.preventDefault();
+	const url = document.querySelector(".playlist-input").value.split("/");
+	const id = url[url.indexOf("playlist") + 1].split("?")[0];
+	fetch(
+		`https://us-central1-centering-star-293515.cloudfunctions.net/getSpotifyData/${id}`,
+	)
+		.then((response) => response.json())
+		.then((data) => console.log(data));
+}
+
 function App() {
 	let playlistEntered = false;
 	if (playlistEntered) {
@@ -17,7 +28,9 @@ function App() {
 						className="playlist-input"
 						placeholder="Enter a Spotify playlist URL"
 					></input>
-					<button className="playlist-submit">Enter</button>
+					<button className="playlist-submit" onClick={(e) => getPlaylist(e)}>
+						Enter
+					</button>
 				</form>
 			</div>
 		);
