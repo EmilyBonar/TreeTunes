214586b1bb1809a74bcdc899a22a6a2673b410c5
emilybonar <emilykbonar@gmail.com>
2020-12-29 13:43:32 -0600

    Added options

--- src/Chart.js
+++ src/Chart.js
@@ -11,7 +11,14 @@ export default function Chart(props) {
 	let elementArray = props.playlist.map((song) => (
 		<ChartElement song={song} feature={feature} vertical={vertical} />
 	));
-	let features = ["danceability", "acousticness", "track length"];
+	let features = [
+		"danceability",
+		"acousticness",
+		"instrumentalness",
+		"liveness",
+		"speechiness",
+		"positivity",
+	];
 	return (
 		<div>
 			<select
@@ -31,6 +38,14 @@ export default function Chart(props) {
 				})}
 			</select>
 			<div className="flex lg:items-end lg:flex-row flex-col lg:divide-x-2 divide-y-2 lg:h-96 h-screen mt-4 lg:absolute lg:bottom-0 lg:w-5/6">
+				<div className="lg:h-full border-gray-600 flex lg:flex-col flex-row-reverse border-b-2 lg:border-b-0 lg:border-r-2 text-right">
+					<p className="flex-grow relative top-0">100%</p>
+					<p className="flex-grow">80%</p>
+					<p className="flex-grow">60%</p>
+					<p className="flex-grow">40%</p>
+					<p className="flex-grow">20%</p>
+					<p className="relative bottom-0">0%</p>
+				</div>
 				{elementArray}
 			</div>
 		</div>
@@ -49,8 +64,17 @@ function ChartElement(props) {
 		case "acousticness":
 			size = song.features.acousticness * 100;
 			break;
-		case "track length":
-			size = song.track.duration_ms / 4000;
+		case "instrumentalness":
+			size = song.features.instrumentalness * 100;
+			break;
+		case "liveness":
+			size = song.features.liveness * 100;
+			break;
+		case "speechiness":
+			size = song.features.speechiness * 100;
+			break;
+		case "positivity":
+			size = song.features.valence * 100;
 			break;
 		default:
 			break;
@@ -60,7 +84,7 @@ function ChartElement(props) {
 			className="bg-blue-400 w-5 flex-grow hover:bg-blue-600 group"
 			style={vertical ? { height: size + "%" } : { width: size + "%" }}
 		>
-			<p className="relative w-max hidden group-hover:block group-hover:bg-white opacity-80 p-2">
+			<p className="relative bottom-10 w-max hidden group-hover:block group-hover:bg-white opacity-80 p-2">
 				{song.track.name}
 			</p>
 		</div>
